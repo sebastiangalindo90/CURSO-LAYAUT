535b3ee34f9c96df4dcf4910b519fa56fa3e6b53
Sebastian Galindo Gallardo <59293335+sebastiangalindo90@users.noreply.github.com>
2023-03-08 16:42:40 -0300

    Initial commit

---
+++ app/src/main/java/com/example/curso_layaut/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.curso_layaut.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
