cbe9423e1823dd0ef911674584c0af557b316ca9
Sebastian <sebastian.galindo.gallardo@gmail.com>
2023-03-09 23:19:46 -0300

    avance en diseño 2023 aprendiendo android layout

--- app/src/main/java/com/example/curso_layaut/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/curso_layaut/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.curso_layaut.databinding.FragmentDashboardBinding
+import com.example.curso_layaut.databinding.FragmentFrameBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFrameBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFrameBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
